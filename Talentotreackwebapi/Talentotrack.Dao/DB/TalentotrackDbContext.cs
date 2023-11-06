afdde6dd5557bc57e24771d6ad50d15622315f1d
siddhesh <siddheshpardeshi01@gmail.com>
2023-11-06 20:17:10 +0530

    update

--- Talentotreackwebapi/Talentotrack.Dao/DB/TalentotrackDbContext.cs
+++ Talentotreackwebapi/Talentotrack.Dao/DB/TalentotrackDbContext.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Talentotrack.Dao.entities;
+using Talentotrack.common.entities;
 
 namespace Talentotrack.Dao.DB
 {
---
+++ Talentotreackwebapi/Talentotrack.Dao/Repositories/AccountRepository.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talentotrack.common.DTOs.Account;
+using Talentotrack.common.entities;
+using Talentotrack.common.Repositories;
+using Talentotrack.Dao.DB;
+
+namespace Talentotrack.Dao.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public TalentotrackDbContext _dbContext;
+
+        public AccountRepository(TalentotrackDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<LoginDetails>GetLoginDetails(string username, string password)
+        {
+            var dbrecord = await _dbContext.tbl_login_details.Where(p => p.username.Equals(username) && p.password.Equals(password)).FirstOrDefaultAsync();
+            return dbrecord;
+        }
+
+    
+    }  
+}
